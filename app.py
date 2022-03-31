42ec151784ceceba1e4b353bf15dd8dc9e108da5
Amb_Olatunji <dareartabefe@gmail.com>
2022-03-31 14:56:36 +0100

    ext

--- app.py
+++ app.py
@@ -397,7 +397,7 @@ def main():
 		st.markdown('Input values in the form below for prediction, Dont mind the long input, just to ensure the right prediction')
 		#loading in the model to predict on the data
 		
-		model_open = open('XGB.ml', 'rb')
+		model_open = open('XGB.sav', 'rb')
 		classifier = joblib.load(model_open)
 
 		if st.button('Show data'):
